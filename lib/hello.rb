ac3353366d4fb2fcd50574066988fd192b8a093d
Scott Ungchusri <animoogle504@gmail.com>
2018-07-09 04:43:45 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,10 +2,12 @@ def hello_t(array)
   i = 0 
   
   while i<array.length 
+    yield array[i]
     i+=1 
   end
 
 end
 
 # call your method here!
-
+myArray = ["Tim", "Tom", "Jim"]
+hello_t(myArray)
